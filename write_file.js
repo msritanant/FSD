f8e8e6d5b857670c0444a68ff0e16597e15ca1a7
msritanant <1ms24mc007@msrit.edu>
2025-07-10 16:10:32 +0530

    Add files via upload

---
+++ write_file.js
@@ -0,0 +1,30 @@
+//Write a pgm to write "Welcome to Node.js"inyo a file named welcome.txt
+//Read the content of welcome.txt and log it usign a callback
+
+// const fs = require('fs');
+
+// fs.writeFile('output.txt', 'Hello, Node!', (err)=>{
+//     if(err) throw err;
+
+//     console.log('File created and written successfully');
+// });
+
+const fs = require('fs');
+
+// Write "Welcome to Node.js" to welcome.txt
+fs.writeFile('welcome.txt', 'Welcome to Node.js', (writeErr) => {
+  if (writeErr) {
+    return console.error('Error writing file:', writeErr);
+  }
+
+  console.log('File written successfully.');
+
+  // Read the content of welcome.txt
+  fs.readFile('welcome.txt', 'utf8', (readErr, data) => {
+    if (readErr) {
+      return console.error('Error reading file:', readErr);
+    }
+    
+    console.log('File content:', data);
+  });
+});
